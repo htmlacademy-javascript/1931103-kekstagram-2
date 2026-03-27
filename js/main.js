c42a6e8ed52c4837217f87b81539f89c86b201ba
ksenya3112 <ks55883112@gmail.com>
2026-03-26 17:51:23 -0700

    Добавляет отрисовку миниатюр

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ import { generatePhotos } from './js/data.js';
 import './js/util.js';
 import './js/data.js';
 
+import { renderThumbnails } from './thumbnails.js';
+renderThumbnails();
+
+
 
 const finalData = generatePhotos();
 
