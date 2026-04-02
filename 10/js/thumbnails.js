6d7f95ba86768ba53988a37f86105b461a71ab78
Keks <keks@htmlacademy.ru>
2026-04-03 01:46:38 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/thumbnails.js
@@ -0,0 +1,35 @@
+import { openFullSizePicture } from './full-size-picture.js';
+
+const thumbnailsContainer = document.querySelector('.pictures');
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderThumbnail = (thumbnailData) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+  const image = thumbnail.querySelector('.picture__img');
+
+  image.src = thumbnailData.url;
+  image.alt = thumbnailData.description;
+  thumbnail.querySelector('.picture__likes').textContent = thumbnailData.likes;
+  thumbnail.querySelector('.picture__comments').textContent = thumbnailData.comments.length;
+
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    openFullSizePicture(thumbnailData);
+  });
+
+  return thumbnail;
+};
+
+const renderThumbnails = (photos) => {
+  const thumbnailFragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    const thumbnail = renderThumbnail(photo);
+    thumbnailFragment.append(thumbnail);
+  });
+
+  thumbnailsContainer.append(thumbnailFragment);
+};
+
+export { renderThumbnails };
