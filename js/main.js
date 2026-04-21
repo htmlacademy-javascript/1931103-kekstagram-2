05d8f6dbb5df6e24f4ef06f6acfc333a4e150ee2
ksenya3112 <ks55883112@gmail.com>
2026-04-20 20:59:47 -0700

    Добавляет задание "Надо подкачаться"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-import { generatePhotos } from './data.js';
 
 import './util.js';
 import './data.js';
 import './form.js';
 
 import { renderThumbnails } from './thumbnails.js';
-renderThumbnails(generatePhotos());
+import { getData } from './api.js';
+import { showAlert } from './util.js';
+import { setUserFormSubmit, closeUploadModal } from './form.js';
+
+getData()
+  .then((photos) => {
+    renderThumbnails(photos);
+  })
+  .catch(() => {
+    showAlert();
+  });
+
+setUserFormSubmit(closeUploadModal);
